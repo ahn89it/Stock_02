1d7e36fc8fd0e58a1b5d530ddf2e95e5803826ba
gunny <ahn89it@gmail.com>
2018-05-15 16:22:45 +0900

    Initial commit

---
+++ test01.py
@@ -0,0 +1,31 @@
+
+import requests
+from bs4 import BeautifulSoup
+import ALL_CODE_LIST
+
+def MAL_Function(x):
+    url = "http://finance.daum.net/item/quote_avg_yyyymmdd_sub2.daum?code={0}".format(x)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    test = soup.select('td span')
+    array = [test[i].text for i in range(len(test))]
+    array2 = []
+    array2.append(int(array[2].replace(",", "")))
+    array2.append(int(array[4].replace(",", "")))
+    cal = (array2[1] / array2[0] - 1) * 100
+    return cal
+
+
+
+for i in range(654,800):
+    print(MAL_Function(ALL_CODE_LIST.ALL_CODE_LIST[i]),i)
+
+# index = 1
+# for i in range(len(ALL_CODE_LIST.ALL_CODE_LIST)):
+#     k=1
+#     if ((MAL_Function(ALL_CODE_LIST.ALL_CODE_LIST[i]) > -0.5) & (MAL_Function(ALL_CODE_LIST.ALL_CODE_LIST[i]) <= 0.5)):
+#         print (ALL_CODE_LIST.ALL_CODE_LIST[i], i, index)
+#         index +=1
+
+
+
